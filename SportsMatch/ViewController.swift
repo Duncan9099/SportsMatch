256426a5465818d49e1afe9c2cd6e6d1c4554244
Duncan Skinner <duncanskinner94@gmail.com>
2019-12-09 12:18:04 +0000

    Initial Commit

---
+++ SportsMatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportsMatch
+//
+//  Created by Duncan Skinner on 9/12/2019.
+//  Copyright © 2019 Duncan Skinner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
